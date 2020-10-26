9acf745ecd010b4a32ce06153886181144c02668
Primoz Susa <primoz.susa@gmail.com>
2020-10-26 13:32:54 +0100

    pwa

--- client/next.config.js
+++ client/next.config.js
@@ -1,20 +1,29 @@
 const withImages = require('next-images');
 const withPreCompression = require('@moxy/next-pre-compression');
+const withPWA = require('next-pwa');
 
-module.exports = withPreCompression(
-  withImages({
-    compress: process.env.NODE_ENV === 'production',
-    fileExtensions: ['jpg', 'jpeg', 'png', 'gif'],
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        issuer: {
-          test: /\.(js|ts)x?$/,
-        },
-        use: ['@svgr/webpack'],
-      });
+const isProd = process.env.NODE_ENV === 'production';
 
-      return config;
-    },
-  })
+module.exports = withPWA(
+  withPreCompression(
+    withImages({
+      pwa: {
+        disable: !isProd,
+        dest: 'public',
+      },
+      compress: isProd,
+      fileExtensions: ['jpg', 'jpeg', 'png', 'gif'],
+      webpack(config) {
+        config.module.rules.push({
+          test: /\.svg$/,
+          issuer: {
+            test: /\.(js|ts)x?$/,
+          },
+          use: ['@svgr/webpack'],
+        });
+
+        return config;
+      },
+    })
+  )
 );
