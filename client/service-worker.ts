7ca58752bd1ed4e7f949f8fecb1e4d5cf7b5559f
Primoz Susa <primoz.susa@gmail.com>
2020-11-15 16:39:49 +0100

    sw update

--- client/service-worker.ts
+++ client/service-worker.ts
@@ -1,38 +1,22 @@
 import { skipWaiting, clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
 import {
-  // NetworkOnly,
   NetworkFirst,
   CacheFirst,
   StaleWhileRevalidate,
 } from 'workbox-strategies';
-import {
-  registerRoute,
-  setDefaultHandler,
-  // setCatchHandler,
-} from 'workbox-routing';
-import {
-  // matchPrecache,
-  precacheAndRoute,
-  cleanupOutdatedCaches,
-} from 'workbox-precaching';
+import { registerRoute, setDefaultHandler } from 'workbox-routing';
+import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
 
 skipWaiting();
 clientsClaim();
-
-// must include following lines when using inject manifest module from workbox
-// https://developers.google.com/web/tools/workbox/guides/precache-files/workbox-build#add_an_injection_point
-// const WB_MANIFEST = self.__WB_MANIFEST;
 
 // @ts-ignore
 // eslint-disable-next-line
 precacheAndRoute(self.__WB_MANIFEST);
 
-// precacheAndRoute(self.__WB_MANIFEST, {
-//   ignoreURLParametersMatching: [/.*/],
-// });
-
 cleanupOutdatedCaches();
+
 registerRoute(
   '/',
   new NetworkFirst({
@@ -47,6 +31,22 @@ registerRoute(
   }),
   'GET'
 );
+
+registerRoute(
+  /\/season\/.*$/i,
+  new NetworkFirst({
+    cacheName: 'season-url',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 3,
+        maxAgeSeconds: 86400,
+        purgeOnQuotaError: !0,
+      }),
+    ],
+  }),
+  'GET'
+);
+
 registerRoute(
   /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
   new CacheFirst({
@@ -75,21 +75,22 @@ registerRoute(
   }),
   'GET'
 );
-// disable image cache, so we could observe the placeholder image when offline
-// registerRoute(
-//   /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
-//   new NetworkOnly({
-//     cacheName: 'static-image-assets',
-//     plugins: [
-//       new ExpirationPlugin({
-//         maxEntries: 64,
-//         maxAgeSeconds: 86400,
-//         purgeOnQuotaError: !0,
-//       }),
-//     ],
-//   }),
-//   'GET'
-// );
+
+registerRoute(
+  /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+  new CacheFirst({
+    cacheName: 'static-image-assets',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 64,
+        maxAgeSeconds: 86400,
+        purgeOnQuotaError: !0,
+      }),
+    ],
+  }),
+  'GET'
+);
+
 registerRoute(
   /\.(?:js)$/i,
   new StaleWhileRevalidate({
@@ -132,36 +133,23 @@ registerRoute(
   }),
   'GET'
 );
-// registerRoute(
-//   /\/api\/.*$/i,
-//   new NetworkFirst({
-//     cacheName: 'apis',
-//     networkTimeoutSeconds: 10,
-//     plugins: [
-//       new ExpirationPlugin({
-//         maxEntries: 16,
-//         maxAgeSeconds: 86400,
-//         purgeOnQuotaError: !0,
-//       }),
-//     ],
-//   }),
-//   'GET'
-// );
-// registerRoute(
-//   /\/api\/.*$/i,
-//   new NetworkFirst({
-//     cacheName: 'apis',
-//     networkTimeoutSeconds: 10,
-//     plugins: [
-//       new ExpirationPlugin({
-//         maxEntries: 16,
-//         maxAgeSeconds: 86400,
-//         purgeOnQuotaError: !0,
-//       }),
-//     ],
-//   }),
-//   'POST'
-// );
+
+registerRoute(
+  /\/scores\/.*$/i,
+  new NetworkFirst({
+    cacheName: 'apis',
+    networkTimeoutSeconds: 10,
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 16,
+        maxAgeSeconds: 86400,
+        purgeOnQuotaError: !0,
+      }),
+    ],
+  }),
+  'GET'
+);
+
 registerRoute(
   /.*/i,
   new NetworkFirst({
@@ -178,44 +166,8 @@ registerRoute(
   'GET'
 );
 
-// following lines gives you control of the offline fallback strategies
-// https://developers.google.com/web/tools/workbox/guides/advanced-recipes#comprehensive_fallbacks
-
 // Use a stale-while-revalidate strategy for all other requests.
 setDefaultHandler(new StaleWhileRevalidate());
-
-// This "catch" handler is triggered when any of the other routes fail to
-// generate a response.
-// setCatchHandler(({ event }) => {
-//   // The FALLBACK_URL entries must be added to the cache ahead of time, either
-//   // via runtime or precaching. If they are precached, then call
-//   // `matchPrecache(FALLBACK_URL)` (from the `workbox-precaching` package)
-//   // to get the response from the correct cache.
-//   //
-//   // Use event, request, and url to figure out how to respond.
-//   // One approach would be to use request.destination, see
-//   // https://medium.com/dev-channel/service-worker-caching-strategies-based-on-request-types-57411dd7652c
-//   switch (event.request.destination) {
-//     // case 'document':
-//     // If using precached URLs:
-//     // return matchPrecache('/fallback');
-//     // return caches.match('/fallback')
-//     // break
-//     // case 'image':
-//     // If using precached URLs:
-//     // return matchPrecache('/static/images/fallback.png');
-//     // return caches.match('/static/images/fallback.png')
-//     // break
-//     // case 'font':
-//     // If using precached URLs:
-//     // return matchPrecache(FALLBACK_FONT_URL);
-//     //return caches.match('/static/fonts/fallback.otf')
-//     //break
-//     default:
-//       // If we don't have a fallback, just return an error response.
-//       return Response.error();
-//   }
-// });
 
 // @ts-ignore
 // eslint-disable-next-line
